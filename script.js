9721f6f0cb371513f8d12586276e8c94c585f625
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-06-27 03:21:04 +0000

    finish intro slides

--- script.js
+++ script.js
@@ -72,6 +72,51 @@ function startIntro () {
     setTimeout(function () {
       $("#islide1").hide();
       $("#islide2").show();
+
+      setTimeout(function () {
+        $("#islide2").hide();
+        $("#islide3").show();
+
+        setTimeout(function () {
+          $("#islide3").hide();
+          $("#islide4").show();
+
+          setTimeout(function () {
+            $("#islide4").hide();
+            $("#islide5").show();
+
+            setTimeout(function () {
+              $("#islide5").hide();
+              $("#islide6").show();
+
+              setTimeout(function () {
+                $("#islide6").hide();
+                $("#islide7").show();
+
+                setTimeout(function () {
+                  $("#islide8").hide();
+                  $("#islide9").show();
+
+                  setTimeout(function () {
+                    $("#islide9").hide();
+                    $("#islide10").show();
+
+                    setTimeout(function () {
+                      $("#islide10").hide();
+                      $("#islide11").show();
+
+                      setTimeout(function () {
+                        $("#islide11").hide();
+                        $("#islide12").show();
+                      }, 3000);
+                    }, 3000);
+                  }, 3000); 
+                }, 6000);
+              }, 4000);
+            }, 6000);
+          }, 2500);
+        }, 6000);
+      }, 4000);
     }, 3000);
   }, 3100);
 }
